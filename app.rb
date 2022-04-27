21a59093fad01479a42cdef1368ad3185ce66821
Artem134134 <car322132@gmail.com>
2022-04-27 11:16:42 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 class Logger
-	def say_something
-		puts "Cool!"
+	def initialize
+		@f = File.open 'log.txt', 'a'
+	end
+
+	# class method
+	def self.say_something
+		puts "Ha-ha!"
+	end
+
+	# instance method
+	def log_something wat
+		@f.puts wat
 	end
 end
 
-Logger.say_something
+Logger.say_something 
+
+logger = Logger.new
+logger.log_something 'hey'
